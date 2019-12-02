44e7c3ababc5965d26415f9dcca8f5468f933a8a
Chillsthrills <pwan.wa100@gmail.com>
2019-12-02 08:46:06 +0300

    New component, entry-form

--- quote/src/app/app.module.ts
+++ quote/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { EntryFormComponent } from './entry-form/entry-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    EntryFormComponent
   ],
   imports: [
     BrowserModule,
