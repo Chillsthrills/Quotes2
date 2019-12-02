21700027095dd3a053b80af61a002346c20f1679
Chillsthrills <pwan.wa100@gmail.com>
2019-12-02 08:54:28 +0300

    Created variables

--- quote/src/app/entry-form/entry-form.component.ts
+++ quote/src/app/entry-form/entry-form.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntryFormComponent implements OnInit {
 
+  author: string;
+  submitted_by: string;
+  new_quote: string;
+  date: Date = new Date();
+  quoteEntry: any;
+
+
+  @Output() mEntry = new EventEmitter<Entry>();
+
+  addnewentry(){
+    this.quoteEntry = new Entry(this.new_quote, this.date, this.author, this.submitted_by);
+    this.mEntry.emit(this.quoteEntry);
+
+  }
+
   constructor() { }
 
   ngOnInit() {
