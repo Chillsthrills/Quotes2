0671bc34e052d024722db7a0f3b9c6c91fb6a554
Chillsthrills <pwan.wa100@gmail.com>
2019-12-02 10:49:58 +0300

    entry-form.component selector listing to parent component

--- quote/src/app/app.module.ts
+++ quote/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 import { DetailComponent } from './detail/detail.component';
 import { TimePipe } from './time.pipe';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { TimePipe } from './time.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
