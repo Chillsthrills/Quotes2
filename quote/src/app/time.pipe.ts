c1706f978787a494fa2bff57447e7d54a983d350
Chillsthrills <pwan.wa100@gmail.com>
2019-12-02 09:34:37 +0300

    edit on time.pipe.ts

--- quote/src/app/time.pipe.ts
+++ quote/src/app/time.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    return null;
+    let currentdate = new Date();
+    let dateinseconds = currentdate.getSeconds();
+    return dateinseconds;
   }
 
 }
